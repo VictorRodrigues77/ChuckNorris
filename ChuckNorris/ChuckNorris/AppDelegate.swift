e6024df26a5a685124800ebb77137400451c871e
Victor Rodrigues <victorrodrigues@MacBook-Pro-de-Victor.local>
2021-10-18 04:17:01 -0300

    Adding initial project

---
+++ ChuckNorris/ChuckNorris/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ChuckNorris
+//
+//  Created by Victor Rodrigues on 18/10/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+    
+}
